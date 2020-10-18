40262ccb60dc7a7acfa97d616affc04a30da6649
Emily Tran <tran.thn@gmail.com>
2020-10-18 00:37:43 -0400

    add name mapping for refs and organism name, polish graph output

--- graph.py
+++ graph.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 import numpy as np
-from io import BytesIO
-import matplotlib.pyplot as plt
-import matplotlib.cbook as cbook
+import matplotlib.pyplot as plot
 import matplotlib
 
 # draws scatter plot representing our fragment recruitment plot
 #
 # arguments
-#   - ref_name: name of reference genome (e.g. NC_1111)
-#   - ref_genome_len: length of reference genome, to be used for x-axis limit
+#   - prefix: name to use for output graph file, which will be <current directory>/out/prefix.png
 #   - positions: flat 1-d array of starting coordinates for aligned sequences from our sample
 #   - identities: flat 1-d array of identities matching above position coordinates
 #
 # returns
 #   - nothing, will save plot to output PNG
-def draw_scatter(ref_name, ref_genome_len, positions, identities):
-    x = positions
-    y = identities
+def draw_scatter(prefix, organism, positions, identities):
+    def f(x):
+        return x / 1000000
+
+    x = np.array(positions)
+    x = np.array([f(xi) for xi in x])
+    y = np.array(identities)
+
     colors = np.random.rand(len(x))
-    area = 2
+    area = 3
 
     ## draw actual scatter plot
-    plt.scatter(x, y, s = area, c = colors, alpha = 0.5, marker = '.')
-    
-    ## set axes
-    plt.xlim(0, ref_genome_len)
-    plt.ylim(0, 1.1)
-    plt.grid()
+    plot.scatter(x, y, s = area, c = colors, alpha = 0.5, marker = '.')
+
+    ## set labels for axes
+    plot.ylabel('% identity')
+    plot.xlabel('position [Mbp]')
+    plot.title(organism)
+
+    ## set axes boundaries
+    ## add a bit of buffer to percent identity for min and max for easier visualization
+    plot.ylim(y.min() - 0.1, 1.0)
+    plot.grid()
+
+    file_name = 'out/' + prefix + '.graph'
 
     # save to output, rather than drawing inline
-    plt.savefig('{0}.png'.format(ref_name), format='png')
+    plot.savefig('{0}.png'.format(file_name), format='png')
--- parse-sam.py
+++ parse-sam.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import json
 import os
-import numpy
 from sam import SAM
 import graph
 
@@ -31,6 +31,10 @@ def process_sam_file(input):
                             length = int(parts[2].split('LN:')[1].strip())
                         else:
                             continue
+                    
+                    # parse raw SAM line using SAM helper class
+                    # we'll pull out the identity and position into a separate array
+                    # to make passing the values into the graph function simpler
                     else:
                         sam = SAM(raw_str = l)
                         sam_reads.append(sam)
@@ -44,28 +48,62 @@ def process_sam_file(input):
 
     return { 'reads': sam_reads, 'coords': coords, 'identities': identities,'len': length, 'ref': ref_name }
 
+# wrapper to call process_sam_file given a directory of SAM files
 def process_sam_file_dir(dir):
     reads = []
     for f in os.listdir(dir):
-        file = os.path.join(dir, f)
-        print('processing:\t{0}'.format(file))
-        reads.append(process_sam_file(file))
+        if (f.endswith('.sam')):
+            file = os.path.join(dir, f)
+            print('processing:\t{0}'.format(file))
+            reads.append(process_sam_file(file))
 
     return reads
 
+# helper to parse name-mapping json, which maps
+# taxon ID x organism name x NCBI accession number
+# SAM files contain the accession number, but we want the organism name
+def get_name_mappings():
+    with open('name-mapping.json') as f:
+        data = json.load(f)
+    
+    name_map = {}
+
+    # convert the json objects to mapped values
+    # of acccession = organism name for easy lookup
+    for d in data:
+        name_map[d['id']] = d['name']
+
+    return name_map
+
+# helper to call graph scatter plot
+def graph_sam_read(read, filename):
+    # since we could have filenames in different directory paths
+    # we only want the base filename without directory to use as prefix
+    fname = os.path.basename(filename)
+    org_name = name_maps[r['ref']]
+
+    print('drawing graph for {0}...'.format(fname))
+    graph.draw_scatter(prefix = fname, organism = org_name, positions = read['coords'], identities = read['identities'])
+
 ### main ###
+global name_maps
+
 if (len(sys.argv) > 1):
     arg = sys.argv[1]
+    name_maps = get_name_mappings()
+
+    # process directory of SAM files, will call the directory wrapper helper
     if os.path.isdir(arg):
         sam_reads = process_sam_file_dir(dir = sys.argv[1])
-        r = sam_reads[0]
-    else:
-        r = process_sam_file(arg)
+        for r in sam_reads:
+            graph_sam_read(r, arg)
 
-    print('drawing graph...')
-    print(r['coords'])
-    print(r['identities'])
-    graph.draw_scatter(r['ref'], r['len'], r['coords'], r['identities'])
+    # process singular SAM file
+    else:
+        if (arg.endswith('.sam')):
+            r = process_sam_file(arg)
+            graph_sam_read(r, arg)
+
     print()
 else:
     print('No file argument was passed, exiting...')
