634f9b227a835388e64060b435774a2f51811f06
Emily Tran <tran.thn@gmail.com>
2020-12-12 20:13:47 -0500

    final: update conversion script to handle specified depth for json

--- convert_phylo_xml.py
+++ convert_phylo_xml.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import sys
 import json
-import xml.etree.cElementTree as etree
-from Bio import Phylo
 import xmltodict
+# import re
+# import xml.etree.cElementTree as etree
+# from Bio import Phylo
+
 
 def xml_to_json(file_name):
     with open(file_name) as f:
@@ -14,20 +15,39 @@ def write_out_json(phylo, fname):
     with open(fname, 'w') as f:
         f.write(phylo)
 
-def rename_dict_key(d, old_key, new_key):
-    for k,v in d.items():
-        if k == old_key:
-            d[new_key] = d.pop(old_key)
-        if isinstance(v, dict):
-            rename_dict_key(v, old_key, new_key)
+def max_depth(root):
+    if 'clade' in root.keys():
+        return 1 + max([0] + list(map(max_depth, root['clade'])))
+    else:
+        return 1
+
+
+def traverse_depth(root, max_depth = None, depth = 0):
+    if 'clade' in root.keys():
+        if (depth == max_depth):
+            root['value'] = 1
+            root.pop('clade')
+        else:
+            depth += 1
+            [traverse_depth(x, max_depth, depth) for x in root['clade']]
+
+    else:
+        root['value'] = 1
 
 ###########################################################################
-# fname = '../data/phyloxml-hmp.xml'
-fname = '../data/test.xml'
-out = xml_to_json(fname)
-rename_dict_key(out, 'clade', 'children')
-print(out)
-unnest = out['phyloxml']['phylogeny']['children']
+input_file = '../data/phyloxml-hmp.xml'
+output_file = 'phylo-test.json'
+# output_file = 'test.json'
+# input_file = '../data/test.xml'
+
+out = xml_to_json(input_file)
+unnest = out['phyloxml']['phylogeny']['clade']
 unnest['name'] = 'root'
+
+traverse_depth(unnest, max_depth = 6, depth = 0)
+# depth = max_depth(unnest)
+# print('max depth', depth)
+
 json = json.dumps(unnest)
-write_out_json(json, 'test_out.json')
+json = json.replace("clade", "children")
+write_out_json(json, output_file)
--- d3-version/main.js
+++ d3-version/main.js
@@ -1,6 +1,8 @@
 // d3.json("flare.json").then(function(data) {
 d3.json("phylo-test.json").then(function(data) {
     console.log(data);
+    child_attr = "value"
+        // child_attr = "branch_length"
 
     width = 932
     radius = width / 6
@@ -16,8 +18,8 @@ d3.json("phylo-test.json").then(function(data) {
 
     partition = data => {
         const root = d3.hierarchy(data)
-            .sum(d => d["@branch_length"])
-            .sort((a, b) => b["@branch_length"] - a["@branch_length"]);
+            .sum(d => d["value"])
+            .sort((a, b) => b["value"] - a["value"]);
         return d3.partition()
             .size([2 * Math.PI, root.height + 1])
             (root);
