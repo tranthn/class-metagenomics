dcafd838f42acb6074dc512804d2bae431aeec47
Emily Tran <tran.thn@gmail.com>
2020-12-14 21:28:05 -0500

    final: add dendrogram d3 sample, clean up stylings for demo, add example plotly html

---
+++ d3-demo/dendrogram.js
@@ -0,0 +1,65 @@
+d3.json('test.json').then(function(data) {
+    width = 1200;
+    radius = width / 2;
+
+    tree = data => {
+        const root = d3.hierarchy(data).sort((a, b) => d3.descending(a.height, b.height) || d3.ascending(a.data.name, b.data.name));
+        root.dx = 10;
+        root.dy = width / (root.height + 1);
+        return d3.cluster().nodeSize([root.dx, root.dy])(root);
+    }
+
+    const root = tree(data);
+    const svg = d3.select('#main').append('svg')
+        .attr('viewBox', [-30, -400, width, width]);
+
+    function autoBox() {
+        document.body.appendChild(this);
+        const { x, y, width, height } = this.getBBox();
+        document.body.removeChild(this);
+        return [x, y, width, height];
+    }
+
+    svg.append('g')
+        .attr('fill', 'none')
+        .attr('stroke', '#555')
+        .attr('stroke-opacity', 0.4)
+        .attr('stroke-width', 1.5)
+        .selectAll('path')
+        .data(root.links())
+        .join('path')
+        .attr('d', d => `
+          M${d.target.y},${d.target.x}
+          C${d.source.y + root.dy / 2},${d.target.x}
+           ${d.source.y + root.dy / 2},${d.source.x}
+           ${d.source.y},${d.source.x}
+        `);
+
+    svg.append('g')
+        .selectAll('circle')
+        .data(root.descendants())
+        .join('circle')
+        .attr('cx', d => d.y)
+        .attr('cy', d => d.x)
+        .attr('fill', d => d.children ? '#0D9595' : '#999')
+        .attr('r', d => d.children ? 5 : 2.5);
+
+    svg.append('g')
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', 7)
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-width', 3)
+        .selectAll('text')
+        .data(root.descendants())
+        .join('text')
+        .attr('x', d => d.y)
+        .attr('y', d => d.x)
+        .attr('dy', '0.31em')
+        .attr('dx', d => d.children ? -6 : 6)
+        .text(d => d.data.name)
+        .filter(d => d.children)
+        .attr('text-anchor', 'end')
+        .clone(true).lower()
+        .attr('stroke', 'white');
+
+});
--- d3-demo/main.js
+++ d3-demo/main.js
@@ -1,10 +1,10 @@
-d3.json("test.json").then(function(data) {
+d3.json('test.json').then(function(data) {
     console.log(data);
-    child_attr = "value"
+    child_attr = 'value'
 
     width = 932
     radius = width / 6
-    format = d3.format(",d")
+    format = d3.format(',d')
 
     arc = d3.arc()
         .startAngle(d => d.x0)
@@ -16,8 +16,8 @@ d3.json("test.json").then(function(data) {
 
     partition = data => {
         const root = d3.hierarchy(data)
-            .sum(d => d["value"])
-            .sort((a, b) => b["value"] - a["value"]);
+            .sum(d => d['value'])
+            .sort((a, b) => b['value'] - a['value']);
         return d3.partition()
             .size([2 * Math.PI, root.height + 1])
             (root);
@@ -29,46 +29,46 @@ d3.json("test.json").then(function(data) {
 
     root.each(d => d.current = d);
 
-    const svg = d3.select('#main').append("svg")
-        .attr("viewBox", [0, 0, width, width])
-        .style("font", "10px sans-serif");
+    const svg = d3.select('#main').append('svg')
+        .attr('viewBox', [0, 0, width, width])
+        .style('font', '10px sans-serif');
 
-    const g = svg.append("g")
-        .attr("transform", `translate(${width / 2},${width / 2})`);
+    const g = svg.append('g')
+        .attr('transform', `translate(${width / 2},${width / 2})`);
 
-    const path = g.append("g")
-        .selectAll("path")
+    const path = g.append('g')
+        .selectAll('path')
         .data(root.descendants().slice(1))
-        .join("path")
-        .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
-        .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
-        .attr("d", d => arc(d.current));
+        .join('path')
+        .attr('fill', d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+        .attr('fill-opacity', d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+        .attr('d', d => arc(d.current));
 
     path.filter(d => d.children)
-        .style("cursor", "pointer")
-        .on("click", clicked);
+        .style('cursor', 'pointer')
+        .on('click', clicked);
 
-    path.append("title")
-        .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+    path.append('title')
+        .text(d => `${d.ancestors().map(d => d.data.name).reverse().join('/')}\n${format(d.value)}`);
 
-    const label = g.append("g")
-        .attr("pointer-events", "none")
-        .attr("text-anchor", "middle")
-        .style("user-select", "none")
-        .selectAll("text")
+    const label = g.append('g')
+        .attr('pointer-events', 'none')
+        .attr('text-anchor', 'middle')
+        .style('user-select', 'none')
+        .selectAll('text')
         .data(root.descendants().slice(1))
-        .join("text")
-        .attr("dy", "0.35em")
-        .attr("fill-opacity", d => +labelVisible(d.current))
-        .attr("transform", d => labelTransform(d.current))
+        .join('text')
+        .attr('dy', '0.35em')
+        .attr('fill-opacity', d => +labelVisible(d.current))
+        .attr('transform', d => labelTransform(d.current))
         .text(d => d.data.name);
 
-    const parent = g.append("circle")
+    const parent = g.append('circle')
         .datum(root)
-        .attr("r", radius)
-        .attr("fill", "none")
-        .attr("pointer-events", "all")
-        .on("click", clicked);
+        .attr('r', radius)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('click', clicked);
 
     function clicked(event, p) {
         parent.datum(p.parent || root);
@@ -86,21 +86,21 @@ d3.json("test.json").then(function(data) {
         // so that if this transition is interrupted, entering arcs will start
         // the next transition from the desired position.
         path.transition(t)
-            .tween("data", d => {
+            .tween('data', d => {
                 const i = d3.interpolate(d.current, d.target);
                 return t => d.current = i(t);
             })
             .filter(function(d) {
-                return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+                return +this.getAttribute('fill-opacity') || arcVisible(d.target);
             })
-            .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
-            .attrTween("d", d => () => arc(d.current));
+            .attr('fill-opacity', d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+            .attrTween('d', d => () => arc(d.current));
 
         label.filter(function(d) {
-                return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+                return +this.getAttribute('fill-opacity') || labelVisible(d.target);
             }).transition(t)
-            .attr("fill-opacity", d => +labelVisible(d.target))
-            .attrTween("transform", d => () => labelTransform(d.current));
+            .attr('fill-opacity', d => +labelVisible(d.target))
+            .attrTween('transform', d => () => labelTransform(d.current));
     }
 
     function arcVisible(d) {
